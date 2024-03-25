7c3f5233ff53ef068bffc80214143d9b7815022c
elasthiel <elasthiel.nolasco@gmail.com>
2024-03-24 22:02:31 -0500

    Initial commit

---
+++ src/FrontEnd/SocialApp/Program.cs
@@ -0,0 +1,33 @@
+using SocialApp.Client.Pages;
+using SocialApp.Router;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SocialApp.Client._Imports).Assembly);
+
+app.Run();
